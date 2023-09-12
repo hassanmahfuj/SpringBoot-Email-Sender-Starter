9f19fcb52ea4ff98cdece670dfb216f3f6f1bde4
Hassan Mahfuj <itsmahfuj@gmail.com>
2023-09-13 03:49:51 +0600

    email html form added on root

--- src/main/java/com/hum/mailer/EmailSenderService.java
+++ src/main/java/com/hum/mailer/EmailSenderService.java
@@ -5,6 +5,12 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
 @Service
 public class EmailSenderService {
 
@@ -13,11 +19,27 @@ public class EmailSenderService {
 
 	public boolean sendEmail(String to, String subject, String body) {
 		SimpleMailMessage m = new SimpleMailMessage();
-		m.setFrom("__@gmail.com");
+		m.setFrom("humarchive1@gmail.com");
 		m.setTo(to);
 		m.setSubject(subject);
 		m.setText(body);
 
+		mailer.send(m);
+		return true;
+	}
+	
+	public boolean sendContentEmail(String to, String subject, String body) {
+		MimeMessage m = mailer.createMimeMessage();
+		try {
+			m.setFrom("humarchive1@gmail.com");
+			m.setRecipient(Message.RecipientType.TO, new InternetAddress(to, false));
+			m.setSubject(subject);
+			m.setContent(body, "text/html");
+		} catch (AddressException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
 		mailer.send(m);
 		return true;
 	}
